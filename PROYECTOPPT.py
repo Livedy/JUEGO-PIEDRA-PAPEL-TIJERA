66a615733bd79dfad3b560c9c3d9faccbd0e3345
Livedy <darwinpilaguano01@gmail.com>
2026-06-18 15:21:39 -0500

    Agregado juego piedra papel tijera con reglas completas

--- PROYECTOPPT.py
+++ PROYECTOPPT.py
@@ -1,1 +1,16 @@
-print ("hola mundo")
+import random
+usuario = input ("escoja piedra papel o tijera: ").lower()
+opciones = ["piedra","papel","tijera"]
+maquina = random.choice(opciones)
+print ("usted eligio", usuario)
+print ("la maquina eligio:", maquina)
+if usuario == maquina:
+ print ("Es un Empate")
+elif usuario == "piedra" and maquina == "tijera":
+ print ("Ganaste la piedra rompe la tijera")
+elif usuario == "papel" and maquina ==  "piedra":
+ print ("Ganaste la piedra envolvio al papel")
+elif usuario == "tijera" and maquina ==  "papel":
+ print ("Ganaste la tijera corto el papel")
+else:
+ print ("La maquina Gana")
